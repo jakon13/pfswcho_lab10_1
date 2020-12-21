1b91cbad9940e76037aa40c7be8cf53e50ec95ca
jakon13 <56788540+jakon13@users.noreply.github.com>
2020-12-21 23:00:11 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main{
-    public static void main(String[] args){
-		  System.out.println("Version 1");
+	public static void main(String[] args){
+		while(true){
+			System.out.println("Version 1");
+		}
 	}
 }
